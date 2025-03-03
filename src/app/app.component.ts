7899a2333854f7aca99c1e1d1f31ff7f9237bcea
jbcharriersofteam <jean-baptiste.charrier@softeam.fr>
2025-03-03 15:04:39 +0100

    NGRX app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,46 +1,16 @@
-import { Component, effect, inject, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { SignalService } from './services/signal.service';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
-import { CommentsComponent } from './comments/comments.component';
+import { Component, OnInit } from '@angular/core';
+import { TodoComponent } from './todo/todo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ReactiveFormsModule, CommentsComponent],
+  imports: [TodoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
-  productService = inject(SignalService);
-  products = this.productService.productsSearched;
-  searchInput: FormControl = new FormControl('');
-  myTitle: any = {
-    name: 'title 1'
-  }
+  title: any = 'My title';
+  constructor() {}
 
-  constructor(){
-    effect(() => {
-      console.log('produit recherché', this.productService.searchProduct());
-    })
-  }
-
-  ngOnInit() {
-    this.productService.getAll();
-    this.searchInput.valueChanges.subscribe((value: string) => {
-      this.productService.setSearchProduct(value as string);
-    });
-  }
-
-  removeProduct(product: any) {
-    this.productService.delete(product);
-  }
-
-  changeTitle(){
-    this.myTitle.name = 'title 2';
-  }
-
-  printComment(event: number){
-    console.log(event);
-  }
+  ngOnInit() {}
 }
---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,42 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { AppState } from '../../ngrx/app.state';
+import { Store } from '@ngrx/store';
+import * as TodoActions from '../../ngrx/todo.actions';
+
+@Component({
+  selector: 'app-todo',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './todo.component.html',
+  styleUrl: './todo.component.scss',
+})
+export class TodoComponent implements OnInit {
+  todos$: Observable<string[]>;
+  newTodo: string = '';
+
+  constructor(private store: Store<AppState>) {
+    this.todos$ = this.store.select((state) => state.todos.todos);
+  }
+
+  ngOnInit() {
+    this.store.dispatch(TodoActions.loadTodos());
+  }
+
+  loadTodos() {
+    this.store.dispatch(TodoActions.loadTodos());
+  }
+
+  addTodo() {
+    if (this.newTodo.trim()) {
+      this.store.dispatch(TodoActions.addTodo({ todo: this.newTodo }));
+      this.newTodo = '';
+    }
+  }
+
+  removeTodo(todo: string) {
+    this.store.dispatch(TodoActions.removeTodo({ todo }));
+  }
+}
